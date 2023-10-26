892937e329e79d1176b32a31c4ad2a73a4044ec7
GutoCugnier <luizcugnier.neto@gmail.com>
2023-10-26 14:54:55 -0300

    att bomb explosion logic

--- input.h
+++ input.h
@@ -12,9 +12,96 @@ using namespace std;
 
 struct Input //Struct para as entradas do usuário
 {   
-    clock_t startTime, endTime, totalTime; //Declaração das entidades de contagem de tempo para armazenar inicio e fim do período da bomba
+    clock_t startTime, endTime, totalTime, powerUpCollectTime, currentTime; //Declaração das entidades de contagem de tempo para armazenar inicio e fim do período da bomba
     char inputKey; //Variável para a entrada do usuário
     bool isGamePaused = false; // Adicione essa variável para rastrear o estado de pausa do jogo
+
+    void collectPowerUp(int **gameMap){
+        if (gameMap[pPlayer->playerX][pPlayer->playerY] == 5) {
+            pPlayer->hasExplosionPowerUp = true;
+            powerUpCollectTime = clock();
+            gameMap[pPlayer->playerX][pPlayer->playerY] = 0;
+        }
+    }
+
+    void explode(int **gameMap, int x, int y) {
+        gameMap[x][y] = 4;
+        if (pPlayer->hasExplosionPowerUp) {
+            // Aumente o raio da explosão aqui
+            spreadExplosion(gameMap, x, y, 2); // Raio maior (2 em vez de 1)
+        } else {
+            spreadExplosion(gameMap, x, y, 1); // Raio normal
+        }
+    }
+
+    void removeExplosion(int **gameMap, int x, int y) {
+        gameMap[x][y] = 0;
+        if (pPlayer->hasExplosionPowerUp) {
+            // Remova a explosão expandida
+            removeSpreadExplosion(gameMap, x, y, 2); // Raio maior
+        } else {
+            removeSpreadExplosion(gameMap, x, y, 1); // Raio normal
+        }
+        bomb.flagBomb = false;
+    }
+
+    void checkPowerUpExpiration() {
+        if (pPlayer->hasExplosionPowerUp && !isGamePaused) {
+            currentTime = clock();
+            double elapsedTime = (double)(currentTime - powerUpCollectTime) / CLOCKS_PER_SEC;
+            cout << "PowerUp Timer =" << elapsedTime << endl;
+            cout << currentTime << powerUpCollectTime;	
+            if (elapsedTime >= 20.0) { // Se passaram 15 segundos
+                pPlayer->hasExplosionPowerUp = false; // Redefina o poder coletado  
+                elapsedTime = 0; // Redefina o tempo decorrido
+            }
+        }
+    }
+
+    void spreadExplosion(int **gameMap, int x, int y, int radius) {
+        // Espalhe verticalmente
+        for (int i = 1; i <= radius; i++) {
+            if (gameMap[x - i][y] == 0 || gameMap[x - i][y] == 2) {
+                gameMap[x - i][y] = 4;
+            }
+            if (gameMap[x + i][y] == 0 || gameMap[x + i][y] == 2) {
+                gameMap[x + i][y] = 4;
+            }
+        }
+
+        // Espalhe horizontalmente
+        for (int i = 1; i <= radius; i++) {
+            if (gameMap[x][y - i] == 0 || gameMap[x][y - i] == 2) {
+                gameMap[x][y - i] = 4;
+            }
+            if (gameMap[x][y + i] == 0 || gameMap[x][y + i] == 2) {
+                gameMap[x][y + i] = 4;
+            }
+        }
+    }
+
+    void removeSpreadExplosion(int **gameMap, int x, int y, int radius) {
+        // Remova a explosão verticalmente
+        for (int i = 1; i <= radius; i++) {
+            if (gameMap[x - i][y] == 4) {
+                gameMap[x - i][y] = 0;
+            }
+            if (gameMap[x + i][y] == 4) {
+                gameMap[x + i][y] = 0;
+            }
+        }
+
+        // Remova a explosão horizontalmente
+        for (int i = 1; i <= radius; i++) {
+            if (gameMap[x][y - i] == 4) {
+                gameMap[x][y - i] = 0;
+            }
+            if (gameMap[x][y + i] == 4) {
+                gameMap[x][y + i] = 0;
+            }
+        }
+    }
+
 
     //Função que posiciona uma bomba no mapa
     void placeBomb(int **gameMap, int x, int y, bool *flagbomb){
@@ -26,9 +113,11 @@ struct Input //Struct para as entradas do usuário
             startTime = clock();
             pBomb->bombX = pPlayer->playerX;
             pBomb->bombY = pPlayer->playerY;
+            
         }
-    }
 
+        pBomb->bombCount++;
+    }
 
     //Função que cuida da logica de explosão da bomba
     void bombExplode(int **gameMap, bool &gameRunning){
@@ -36,21 +125,13 @@ struct Input //Struct para as entradas do usuário
             endTime = clock();
             totalTime = (endTime-startTime)/CLOCKS_PER_SEC;
             cout << totalTime;
-            if ( totalTime == 3) {
-                gameMap[pBomb->bombX][pBomb->bombY] = 4;
-                if (gameMap[pBomb->bombX+1][pBomb->bombY]==0 || gameMap[pBomb->bombX+1][pBomb->bombY]==2) gameMap[pBomb->bombX+1][pBomb->bombY] = 4;
-                if (gameMap[pBomb->bombX-1][pBomb->bombY]==0 || gameMap[pBomb->bombX-1][pBomb->bombY]==2) gameMap[pBomb->bombX-1][pBomb->bombY] = 4;
-                if (gameMap[pBomb->bombX][pBomb->bombY+1]==0 || gameMap[pBomb->bombX][pBomb->bombY+1]==2) gameMap[pBomb->bombX][pBomb->bombY+1] = 4;
-                if (gameMap[pBomb->bombX][pBomb->bombY-1]==0 || gameMap[pBomb->bombX][pBomb->bombY-1]==2) gameMap[pBomb->bombX][pBomb->bombY-1] = 4;
+            if (totalTime == 3) {
+                explode(gameMap, pBomb->bombX, pBomb->bombY);
             }
 
             if (totalTime == 4) {
-                gameMap[pBomb->bombX][pBomb->bombY] = 0;
-                if (gameMap[pBomb->bombX+1][pBomb->bombY]!=1) gameMap[pBomb->bombX+1][pBomb->bombY] = 0;
-                if (gameMap[pBomb->bombX-1][pBomb->bombY]!=1) gameMap[pBomb->bombX-1][pBomb->bombY] = 0;
-                if (gameMap[pBomb->bombX][pBomb->bombY+1]!=1) gameMap[pBomb->bombX][pBomb->bombY+1] = 0;
-                if (gameMap[pBomb->bombX][pBomb->bombY-1]!=1) gameMap[pBomb->bombX][pBomb->bombY-1] = 0;
-                bomb.flagBomb = false;
+                removeExplosion(gameMap, pBomb->bombX, pBomb->bombY);
+                totalTime = 0;
             }
         }
     }
